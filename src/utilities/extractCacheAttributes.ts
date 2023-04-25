cd10ce78e3977315fdd79bcd57f2093732f1cce4
Gajus Kuizinas <gajus@gajus.com>
2023-04-25 15:14:40 -0600

    feat: separate value hash and body hash BREAKING CHANGE: separate key creation from parameter extraction

--- src/utilities/extractCacheAttributes.ts
+++ src/utilities/extractCacheAttributes.ts
@@ -4,6 +4,7 @@ import {
 import {
   type PrimitiveValueExpression,
 } from 'slonik';
+import stripComments from 'strip-comments';
 
 const hash = (subject: string) => {
   return createHash('sha256').update(subject).digest('hex');
@@ -12,20 +13,27 @@ const hash = (subject: string) => {
 export const extractCacheAttributes = (subject: string, values: readonly PrimitiveValueExpression[]) => {
   const ttl = /-- @cache-ttl (\d+)/u.exec(subject)?.[1];
 
+  let valueHash: string | null = null;
+
+  // https://github.com/jonschlinkert/strip-comments/issues/71
+  const bodyHash = hash(stripComments(subject).replaceAll(/^\s*--.*$/ugm, '').replaceAll(/\s/gu, ''));
+
   if (ttl) {
-    let key = /-- @cache-key ([a-zA-Z0-9\-_:/]+)/ui.exec(subject)?.[1];
+    const key = /-- @cache-key ([a-zA-Z0-9\-_:/]+)/ui.exec(subject)?.[1];
 
     if (!key) {
       throw new Error('@cache-key must be specified when @cache-ttl is specified.');
     }
 
     if (!/-- @cache-hash-values false/ui.test(subject) && values.length > 0) {
-      key += ':' + hash(JSON.stringify(values));
+      valueHash = hash(JSON.stringify(values));
     }
 
     return {
+      bodyHash,
       key,
       ttl: Number(ttl),
+      valueHash,
     };
   }
 
